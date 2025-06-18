eb1c4f2b3f1a888927777e8e34d3d6de53c1df78
haldomae <hal.domae@gmail.com>
2025-06-18 21:54:48 +0900

    DAOを追加

---
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/TaskDao.kt
@@ -0,0 +1,22 @@
+package com.classnumber_00_domaekazuki.st42_kadai04
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert
+    fun insertTask(task: Task)
+
+    @Query("SELECT * FROM Task")
+    fun loadAllTasks() : List<Task>
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteTask(task: Task)
+}
