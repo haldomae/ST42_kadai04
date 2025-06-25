1d7f0d8b428396a940758e1b678767d99ff1ec61
haldomae <hal.domae@gmail.com>
2025-06-25 21:02:44 +0900

    不要なコードを削除

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
@@ -19,29 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ST42_kadai04Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ST42_kadai04Theme {
-        Greeting("Android")
     }
 }
