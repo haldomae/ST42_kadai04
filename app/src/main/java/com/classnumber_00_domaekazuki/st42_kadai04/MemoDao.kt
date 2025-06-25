0521246e583adbf2549792d32c27a011ce89df27
haldomae <hal.domae@gmail.com>
2025-06-25 21:08:21 +0900

    DAOを作成

---
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MemoDao.kt
@@ -0,0 +1,23 @@
+package com.classnumber_00_domaekazuki.st42_kadai04
+
+import androidx.room.*
+
+// @Dao = このインターフェースがデータアクセスオブジェクトだと宣言
+@Dao
+interface MemoDao {
+
+    // 🔍 データを「取得」する操作
+    // @Query = SQLクエリ（データベース言語）を書く場所
+    @Query("SELECT * FROM memo")  // 「memoテーブルから全部取ってきて」という意味
+    suspend fun getAll(): List<Memo>  // 結果はMemoのリストで返される
+
+    // 🔍 データを「保存」する操作
+    // @Insert = 新しいデータをテーブルに追加する
+    @Insert
+    suspend fun insert(memo: Memo)  // Memoオブジェクトを受け取って保存
+
+    // 🔍 データを「削除」する操作
+    // @Delete = 指定されたデータをテーブルから削除する
+    @Delete
+    suspend fun delete(memo: Memo)  // 削除したいMemoオブジェクトを指定
+}
