60fcedde7e94dd4f750f9c983b16bd4f906d7454
haldomae <hal.domae@gmail.com>
2025-06-26 11:35:37 +0900

    アプリで使う変数の用意

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.classnumber_00_domaekazuki.st42_kadai04.ui.theme.ST42_kadai04Theme
@@ -17,10 +21,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // データベースを作成(アプリ起動時に1度だけ実行)
+        // AppDatabaseで作った関数を呼び出す
+        val database = getDatabase(this)
+
         setContent {
             ST42_kadai04Theme {
-
+                MemoApp(database)
             }
         }
     }
+}
+@Composable
+fun MemoApp(database : AppDatabase){
+    // 画面の状態を管理する変数たち(ViewModelに入れた方がよい)
+    // 現在表示されているメモのリスト
+    val memos by remember { mutableStateOf(listOf<Memo>()) }
+    // ユーザが入力中のテキスト
+    val newMemoText by remember { mutableStateOf("") }
+
+    // 非同期の処理(DB処理)を実行する為の道具
+    val scope = rememberCoroutineScope()
 }
