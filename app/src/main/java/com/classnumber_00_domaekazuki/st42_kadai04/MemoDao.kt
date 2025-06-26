9c2e71c360c1d1d15fc84e066bf60efd5cf0a46d
haldomae <hal.domae@gmail.com>
2025-06-26 10:26:03 +0900

    Daoを作成

---
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MemoDao.kt
@@ -0,0 +1,24 @@
+package com.classnumber_00_domaekazuki.st42_kadai04
+
+import androidx.room.* // Roomの機能を全部使えるようにする
+
+@Dao
+interface MemoDao {
+    // データを取得する(SELECT)
+    // @QueryはSQLを記述する
+    @Query("SELECT * FROM memo")
+    // suspendを付けると、非同期でDBの処理が行われる
+    suspend fun getAll(): List<Memo>
+
+    // データを保存する(INSERT)
+    // @InsertはSQL記述しなくてよい
+    @Insert
+    // メモオブジェクトを受け取って保存
+    suspend fun insert(memo: Memo)
+    
+    // データを削除する(DELETE)
+    // @DeleteはSQL記述しなくてもよい
+    @Delete
+    // メモオブジェクトを受け取って削除する
+    suspend fun delete(memo: Memo)
+}
