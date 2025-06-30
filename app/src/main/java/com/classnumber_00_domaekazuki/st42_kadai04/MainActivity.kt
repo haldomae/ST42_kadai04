257014eaa1727b3f7d83da792a9304e4d122f953
haldomae <hal.domae@gmail.com>
2025-06-30 11:53:56 +0900

    メモ登録機能を追加

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,8 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.classnumber_00_domaekazuki.st42_kadai04.ui.theme.ST42_kadai04Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +49,7 @@ fun MemoApp(database: AppDatabase){
     // 現在表示されているメモのリスト
     var memos by remember { mutableStateOf(listOf<Memo>()) }
     // ユーザが入力中のテキスト
-    val newMemoText by remember { mutableStateOf("") }
+    var newMemoText by remember { mutableStateOf("") }
     // 非同期で処理(DB処理)を実行する為の道具
     val scope = rememberCoroutineScope()
 
@@ -50,5 +58,58 @@ fun MemoApp(database: AppDatabase){
     LaunchedEffect(Unit) {
         // メモの全データを取得する
         memos = database.memoDao().getAll()
+    }
+
+    // 画面のレイアウト
+    Column(
+        modifier = Modifier
+            .fillMaxSize() // 画面全体を使用
+            .padding(16.dp)
+    ) {
+        // アプリのタイトル
+        Text(
+            text = "メモ",
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(16.dp)
+        )
+        // メモを入力する箇所
+        Row {
+            TextField(
+                // 現在の入力内容
+                value = newMemoText,
+                // 入力欄の内容が変わった時
+                onValueChange = {
+                    newMemoText = it
+                },
+                label = {
+                    Text(
+                        text = "新しいメモ"
+                    )
+                }
+            )
+            Button(
+                // ボタンが押されたとき
+                onClick = {
+                    // 入力欄が空ではないとき
+                    if(newMemoText.isNotBlank()){
+                        scope.launch {
+                            // データベースにメモを登録
+                            database.memoDao().insert(
+                                memo = Memo(text = newMemoText)
+                            )
+                            // 画面の状態を最新にしておく
+                            database.memoDao().getAll()
+                            // 入力欄を空
+                            newMemoText = ""
+                        }
+
+                    }
+                }
+            ) {
+                Text(
+                    text = "登録"
+                )
+            }
+        }
     }
 }
