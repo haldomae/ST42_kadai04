02a311493bc29c91a255359b1cb5b63084a3b83a
haldomae <hal.domae@gmail.com>
2025-06-30 11:35:03 +0900

    アプリ起動時にDBからメモデータを取得

--- app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st42_kadai04/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.classnumber_00_domaekazuki.st42_kadai04.ui.theme.ST42_kadai04Theme
@@ -37,9 +39,16 @@ class MainActivity : ComponentActivity() {
 fun MemoApp(database: AppDatabase){
     // 画面の状態を管理する変数達(ViewModelに入れた方がいい)
     // 現在表示されているメモのリスト
-    val memos by remember { mutableStateOf(listOf<Memo>()) }
+    var memos by remember { mutableStateOf(listOf<Memo>()) }
     // ユーザが入力中のテキスト
     val newMemoText by remember { mutableStateOf("") }
     // 非同期で処理(DB処理)を実行する為の道具
     val scope = rememberCoroutineScope()
+
+    // アプリ起動時にDBからメモを読み取る
+    // アプリ起動時に一度だけ実行
+    LaunchedEffect(Unit) {
+        // メモの全データを取得する
+        memos = database.memoDao().getAll()
+    }
 }
